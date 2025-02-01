2f6e5973779b6768d1923ec6b1b1ba17f8a1246f
bsc-com-16-20 <bsc-com-16-20@unima.ac.mw>
2025-02-01 13:22:14 +0200

    created achievements  page for the donor

--- lib/pages/widgets/achievements.dart
+++ lib/pages/widgets/achievements.dart
@@ -1,10 +1,42 @@
-// lib/widgets/achievements.dart
 import 'package:flutter/material.dart';
 
 class Achievements extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
-  }
+ const Achievements({super.key});
+
+ @override
+ Widget build(BuildContext context) {
+   return Scaffold(
+     body: Padding(
+       padding: EdgeInsets.all(16.0),
+       child: Column(
+         children: [
+           Card(
+             child: ListTile(
+               leading: CircleAvatar(
+                 backgroundColor: Color.fromARGB(255, 255, 243, 194), // Light yellow background
+                 child: Icon(
+                   Icons.emoji_events,
+                   color: Colors.amber, // Trophy color
+                 ),
+               ),
+               title: Text(
+                 'Regular Donor',
+                 style: TextStyle(
+                   fontSize: 16.0,
+                   fontWeight: FontWeight.w500,
+                 ),
+               ),
+               subtitle: Text(
+                 '5+ donations',
+                 style: TextStyle(
+                   color: Colors.grey,
+                 ),
+               ),
+             ),
+           ),
+         ],
+       ),
+     ),
+   );
+ }
 }

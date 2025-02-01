58d8857004e08818aa2bfa4d86c65be85525ad89
bsc-com-16-20 <bsc-com-16-20@unima.ac.mw>
2025-02-01 13:19:40 +0200

    created support section page

--- lib/pages/widgets/support_section.dart
+++ lib/pages/widgets/support_section.dart
@@ -1,11 +1,47 @@
-// lib/widgets/support_section.dart
 import 'package:flutter/material.dart';
 
 class SupportSection extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
-  }
- 
+ const SupportSection({super.key});
+
+ @override
+ Widget build(BuildContext context) {
+   return Scaffold(
+     body: Padding(
+       padding: EdgeInsets.all(16.0),
+       child: Column(
+         children: [
+           Card(
+             child: ListTile(
+               title: Text(
+                 'View FAQs',
+                 style: TextStyle(
+                   fontSize: 16.0,
+                 ),
+               ),
+               trailing: Icon(Icons.arrow_forward_ios, size: 16),
+               onTap: () {
+                 // Handle FAQ tap
+               },
+             ),
+           ),
+           SizedBox(height: 16.0),
+           Card(
+             child: ListTile(
+               title: Text(
+                 'Contact Support',
+                 style: TextStyle(
+                   fontSize: 16.0,
+                 ),
+               ),
+               trailing: Icon(Icons.arrow_forward_ios, size: 16),
+               onTap: () {
+                 // Handle Contact Support tap
+               },
+             ),
+           ),
+         ],
+       ),
+     ),
+   );
+ }
 }

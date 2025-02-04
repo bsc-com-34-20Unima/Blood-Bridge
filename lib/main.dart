f2aed6bbaf16b623d7795779e415f327cc6b80a3
bsc-com-09-20 <bsc-com-09-20@unima.ac.mw>
2025-02-04 18:14:24 +0000

    creating donor ui then Added update and delete functionality for donor

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:bloodbridge/pages/DashboardPages/BloodInventory.dart';
 import 'package:bloodbridge/pages/DashboardPages/BloodRequests.dart';
 import 'package:bloodbridge/pages/DashboardPages/DashboardPage.dart';
-import 'package:bloodbridge/pages/DashboardPages/Donors.dart';
+import 'package:bloodbridge/pages/DashboardPages/Donors.dart'; // Updated import
 import 'package:bloodbridge/pages/DashboardPages/Events.dart';
 import 'package:bloodbridge/pages/login.dart';
 import 'package:flutter/material.dart';
@@ -13,20 +13,18 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: LoginScreen(),
-        routes: {
-      '/dashboard': (context) => DashboardPage(),
-      '/bloodInventory': (context) => BloodInventoryPage(),
-      '/donors': (context) => DonorsPage(),
-      '/requests': (context) => BloodRequests(),
-      '/events': (context) => EventsPage(),
-    },
+      debugShowCheckedModeBanner: false,
+      home: LoginScreen(),
+      routes: {
+        '/dashboard': (context) => DashboardPage(),
+        '/bloodInventory': (context) => BloodInventoryPage(),
+        '/donors': (context) => DonorsPage(), // Updated this line
+        '/requests': (context) => BloodRequests(),
+        '/events': (context) => EventsPage(),
+      },
     );
   }
 }
-

8180f5818132aa1ca2604f0e76b06982c5ef3335
bsc-com-16-20 <bsc-com-16-20@unima.ac.mw>
2025-04-22 03:49:13 +0200

    made the urgent request to be showing to the  blood donor requested and also the pending request to be updated on the hospital dashboard

--- lib/services/bloodrequest_service.dart
+++ lib/services/bloodrequest_service.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 import 'package:bloodbridge/services/auth_service.dart';
 import 'package:http/http.dart' as http;
-import 'package:shared_preferences/shared_preferences.dart';
+
 
 class BloodRequestService {
   final AuthService _authService = AuthService();
